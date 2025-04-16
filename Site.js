ca5870c006a269f5ced98e237372210e8a343d45
Oliveiraxis7 <gb1212go@gmail.com>
2025-04-16 16:32:53 -0300

    site completin manekkk

---
+++ Site.js
@@ -0,0 +1,4 @@
+// Exemplo de função para alertar quando o botão "Jogar" for clicado
+document.getElementById("play").addEventListener("click", function() {
+    alert("Prepare-se para jogar!");
+});
