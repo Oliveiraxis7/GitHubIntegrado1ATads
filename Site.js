5026626b61df2e82abc2e9d1274b267126ac98e2
Oliveiraxis7 <gb1212go@gmail.com>
2025-04-16 16:35:41 -0300

    organizei as parada kjkk

--- Site.js
+++ Site.js
@@ -1,4 +1,4 @@
 // Exemplo de função para alertar quando o botão "Jogar" for clicado
 document.getElementById("play").addEventListener("click", function() {
-    alert("Prepare-se para jogar!");
+    alert("MOJANG E MEU OVUUUUU");
 });
