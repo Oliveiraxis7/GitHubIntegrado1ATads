e6e0ec86b2e55b217417cc7382a88cfc55909348
Oliveiraxis7 <gb1212go@gmail.com>
2025-04-17 15:31:29 -0300

    Site Atualizado, talvez o projeto esteja no fim

--- Site.js
+++ Site.js
@@ -1,5 +1,5 @@
 // Exemplo de função para alertar quando o botão "Jogar" for clicado
 document.getElementById("play").addEventListener("click", function() {
-    alert("MOJANG E MEU OVUUUUU");
+    alert("MOJANG E MEU OVUUUUUO");
 });
 
